2df9e9f6039a3a6f8c6cbb9e8aa5ca5442fda932
Michael Neil <mneil@mneilsworld.com>
2021-01-23 16:23:12 -0800

    Add stack synthensis in bin/cli.js
    For now we're just proxying to aws CDK cli command
    but in the future we'll wrap with our own tools and
    call synth as needed.

--- bin/app.js
+++ bin/app.js
@@ -2,6 +2,7 @@
 
 const cdk = require('@aws-cdk/core');
 const { Stack } = require('../lib/index');
+const debug = require('debug')('cfnsane:cli')
 
 const app = new cdk.App();
 new Stack(app, 'Stack');
