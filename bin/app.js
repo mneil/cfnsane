9fa50aff23b4a002ab4807e2d9f8a5eca3c2b6e9
Michael Neil <mneil@mneilsworld.com>
2021-01-31 15:16:30 -0800

    Add processing of environment and map to config

--- bin/app.js
+++ bin/app.js
@@ -2,7 +2,26 @@
 const cdk = require('@aws-cdk/core');
 const { Stack, attach } = require('../lib/index');
 
+/**
+ * Given an object returns another object
+ * by filtering out any keys in the object
+ * that start with CFNSANE_ and removing
+ * the prefix and lowercasing the rest.
+ * @param {object} env
+ */
+function loadConfig(env) {
+    let loaded = {}
+    for ([key, value] in Object.entries(env)) {
+        if (!key.startsWith('CFNSANE_')){
+            continue;
+        };
+        key = key.replace('CFNSANE_', '');
+        loaded[key] = value;
+    }
+    return loaded;
+}
+const config = loadConfig(process.env);
 const app = new cdk.App();
-const stack = new Stack(app, 'Stack');
+const stack = new Stack(app, 'Stack', config);
 
 attach(stack);
